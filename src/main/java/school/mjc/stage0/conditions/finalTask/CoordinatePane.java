6154677f6e704bde165e9c5aa60e02a078b01844
Zhuravlll <vanya7zhuravlov@gmail.com>
2023-04-13 09:04:17 +0300

    Module #4.1, completed final tasks.

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+        if (y > 0) {
+            switch (x) {
+                case 1 -> System.out.println("first");
+                case -1 -> System.out.println("second");
+            }
+        } else if (x == 0 && y == 0) {
+            System.out.println("zero");
+        } else {
+            switch (x) {
+                case 1 -> System.out.println("fourth");
+                case -1 -> System.out.println("third");
+            }
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,17 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        int leap = year / 100;
+        if (year < 0 || month < 1 || month > 12) {
+            System.out.println("invalid date");
+        } else if ((leap % 2 == 0) && (year % 4 == 0) && (month == 2)) {
+            System.out.println("29");
+        } else {
+            switch (month) {
+                case 1, 3, 5, 7, 8, 10, 12 -> System.out.println("31");
+                case 2 -> System.out.println("28");
+                default -> System.out.println("30");
+            }
+        }
     }
 }
